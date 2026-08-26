cea5dd14001f3b62ff4e599b2b4284b68455863f
Leisy Massiel <leisy.leon@tecsup.edu.pe>
2026-08-26 17:34:54 -0500

    Verificar servidor PostgreSQL

--- index-post.php
+++ index-post.php
@@ -2,16 +2,19 @@
 include("conexion.php");
 $con = conexion();
 
-$resultado = pg_query(
-    $con,
-    "SELECT table_schema, table_name
-     FROM information_schema.tables
-     WHERE table_name = 'persona'"
-);
+$sql = "
+SELECT
+    current_database(),
+    current_user,
+    current_schema(),
+    inet_server_addr()
+";
 
-while ($fila = pg_fetch_assoc($resultado)) {
-    echo $fila["table_schema"] . "." . $fila["table_name"];
-}
+$resultado = pg_query($con, $sql);
+$fila = pg_fetch_assoc($resultado);
 
-exit;
+echo "Base de datos: " . $fila["current_database"] . "<br>";
+echo "Usuario: " . $fila["current_user"] . "<br>";
+echo "Esquema: " . $fila["current_schema"] . "<br>";
+echo "Servidor: " . $fila["inet_server_addr"] . "<br>";
 ?>
